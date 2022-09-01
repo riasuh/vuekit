4ff534c1dfc3d1e15b162c819b768875f54beb30
riasuh <milo099sas@gmail.com>
2022-09-01 09:20:56 +0200

    sewey

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,16 +1,13 @@
 import { defineUserConfig } from 'vuepress'
+import { getDirname, path } from '@vuepress/utils'
+
+const __dirname = getDirname(import.meta.url)
 
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Hello VuePress',
   description: 'Just playing around',
-})
-import { getDirname, path } from '@vuepress/utils'
-
-const __dirname = getDirname(import.meta.url)
-
-export default {
   alias: {
     '@images': path.resolve(__dirname, './docs/.assets/images'),
   },
-}
+})
